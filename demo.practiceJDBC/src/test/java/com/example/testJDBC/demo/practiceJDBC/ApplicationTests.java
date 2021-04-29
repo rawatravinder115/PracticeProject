92b2e6834356333652e6cd728fcebd0b59fa459c
rawatravinder115 <rawatravinder115@gmail.com>
2021-04-29 10:21:50 +0530

    First commit

---
+++ demo.practiceJDBC/src/test/java/com/example/testJDBC/demo/practiceJDBC/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testJDBC.demo.practiceJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
