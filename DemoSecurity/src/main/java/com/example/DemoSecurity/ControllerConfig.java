ac8b71e01b8a9c5148ccd04ad11b1b3ca70b2700
rawatravinder115 <rawatravinder115@gmail.com>
2021-06-02 11:21:39 +0530

    add controller config

--- DemoSecurity/src/main/java/com/example/DemoSecurity/ControllerConfig.java
+++ DemoSecurity/src/main/java/com/example/DemoSecurity/ControllerConfig.java
@@ -3,21 +3,8 @@ package com.example.DemoSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 public class ControllerConfig {
 
-    @GetMapping("/visitor")
-    public String visitor(){
-       return" <h1>hello visitor you dont need authentication</h1> ";
-    }
 
-    @GetMapping("/employee")
-    public String employee(){
-        return "<h1>Hello employee </h1>";
-    }
-
-    @GetMapping("/admin")
-    public String admin(){
-        return "<h1>hello admin<h1>";
-    }
 }
