86af382117bbef4b70e7a08a9062605d5c6711f6
rawatravinder115 <rawatravinder115@gmail.com>
2021-06-02 11:33:34 +0530

    add Config roles to the user

--- DemoSecurity/src/main/java/com/example/DemoSecurity/ControllerConfig.java
+++ DemoSecurity/src/main/java/com/example/DemoSecurity/ControllerConfig.java
@@ -1,10 +1,35 @@
 package com.example.DemoSecurity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
-public class ControllerConfig {
+@EnableWebSecurity
+public class ControllerConfig extends WebSecurityConfigurerAdapter {
 
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().
+                withUser("ravi")
+                .password("ravi")
+                .roles("employee")
+                .and()
+                .withUser("kavi")
+                .password("kavi")
+                .roles("admin");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                .antMatchers("/employee/**").hasRole("employee")
+                .antMatchers("/admin/**").hasAnyRole("admin","employee")
+                .antMatchers("/visitor**").permitAll()
+                .and()
+                .formLogin();
+    }
 }
