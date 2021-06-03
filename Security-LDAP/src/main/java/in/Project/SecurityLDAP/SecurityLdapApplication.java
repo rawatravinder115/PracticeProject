8386c6209f9e041ec9b2aba3465b219ad0176457
rawatravinder115 <rawatravinder115@gmail.com>
2021-06-03 12:41:38 +0530

    add security project - LDAP

---
+++ Security-LDAP/src/main/java/in/Project/SecurityLDAP/SecurityLdapApplication.java
@@ -0,0 +1,13 @@
+package in.Project.SecurityLDAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLdapApplication.class, args);
+	}
+
+}
---
+++ Security-LDAP/src/test/java/in/Project/SecurityLDAP/SecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package in.Project.SecurityLDAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
