671843b66779f2eee8377f0959d798aaaced7702
rawatravinder115 <rawatravinder115@gmail.com>
2021-06-04 02:14:37 +0530

    add project - spring security

---
+++ Security-JWT/src/test/java/in/project/SecurityJWT/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package in.project.SecurityJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
